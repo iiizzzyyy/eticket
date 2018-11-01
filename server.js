49e7eabc32bebd3e1dfaf9e860e51bf17ce89d63
Augusto Schiavi <A.Schiavi@gmail.com>
2018-11-01 00:30:31 -0300

    Added dev and prod envs for server

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const config = yenv('config.yaml');
 const restifyPromise = require('restify-await-promise');
 const eTicketController = require('./api/controllers/eTicket.js');
 
-const { KEY, CRT, PORT } = config
-const serverOptions = { key: fs.readFileSync(KEY), certificate: fs.readFileSync(CRT) };
+const { env: { NODE_ENV } } = process;
+const { KEY, CRT, PORT } = config;
+const isProduction = NODE_ENV == 'production';
+const key = fs.existsSync(KEY) && fs.readFileSync(KEY);
+const certificate = fs.existsSync(CRT) && fs.readFileSync(CRT);
+const serverOptions = isProduction ? { key, certificate } : {};
 const server = restify.createServer(serverOptions);
 
 // SERVER CONFIG
