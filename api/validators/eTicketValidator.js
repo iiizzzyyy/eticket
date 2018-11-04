fe0d86ccefb218d35e265c9b284ca860adb2d8b7
Augusto Schiavi <A.Schiavi@gmail.com>
2018-11-03 21:52:12 -0300

    Added Auth with signed certificate

--- api/validators/eTicketValidator.js
+++ api/validators/eTicketValidator.js
@@ -5,8 +5,8 @@ const { validateTimestamp } = dateHelper;
 
 module.exports = {
     validateParams: (params, next) => {
-        const emailRequiredError = new error.MissingParameterError('email is required');
-        const wrongDateFormatError = new error.UnprocessableEntityError('date must be timestamp in miliseconds');
+        const emailRequiredError = new error.MissingParameterError('Parameter email is required');
+        const wrongDateFormatError = new error.UnprocessableEntityError('Dates must be a timestamp in miliseconds');
         const { email, closedate, departure_date, arrival_date } = params;
 
         !email && next(emailRequiredError);
--- server.js
+++ server.js
@@ -6,16 +6,20 @@ const restifyPromise = require('restify-await-promise');
 const eTicketController = require('./api/controllers/eTicketController.js');
 
 const { env: { NODE_ENV } } = process;
-const { KEY, CRT, PORT } = config;
+const { KEY, CRT, PORT, CA } = config;
 const isProduction = NODE_ENV == 'production';
 const key = fs.existsSync(KEY) && fs.readFileSync(KEY);
 const certificate = fs.existsSync(CRT) && fs.readFileSync(CRT);
-const serverOptions = isProduction ? { key, certificate } : {};
+const ca = fs.existsSync(CA) && fs.readFileSync(CA);
+const requestCert = true;
+const rejectUnauthorized = true;
+const serverOptions = isProduction ? { key, certificate, ca, requestCert, rejectUnauthorized} : {};
 const server = restify.createServer(serverOptions);
 
 // SERVER CONFIG
 server.use(restify.plugins.bodyParser());
 restifyPromise.install(server);
+
 // ENDPOINTS
 server.post('/eticket', eTicketController.post);
 // SERVER START
