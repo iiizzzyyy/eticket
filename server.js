5c2a7e8ffefc5edc0937c168c165b42a803566d7
Augusto Schiavi <A.Schiavi@gmail.com>
2018-11-04 20:07:24 -0300

    Removed unnecessary config

--- server.js
+++ server.js
@@ -5,15 +5,13 @@ const config = yenv('config.yaml');
 const restifyPromise = require('restify-await-promise');
 const eTicketController = require('./api/controllers/eTicketController.js');
 
-const { env: { NODE_ENV } } = process;
 const { KEY, CRT, PORT, CA } = config;
-const isProduction = NODE_ENV == 'production';
 const key = fs.existsSync(KEY) && fs.readFileSync(KEY);
 const certificate = fs.existsSync(CRT) && fs.readFileSync(CRT);
 const ca = fs.existsSync(CA) && fs.readFileSync(CA);
 const requestCert = true;
 const rejectUnauthorized = true;
-const serverOptions = isProduction ? { key, certificate, ca, requestCert, rejectUnauthorized} : {};
+const serverOptions = { key, certificate, ca, requestCert, rejectUnauthorized };
 const server = restify.createServer(serverOptions);
 
 // SERVER CONFIG
